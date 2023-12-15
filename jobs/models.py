2208f948be7d9b411dbac39666210f75bb9f9941
DaveBBL <dave.bbl@gmail.com>
2023-12-14 21:50:44 -0800

    Add Models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=100)
+    industry = models.CharField(max_length=100)
+    location = models.CharField(max_length=50)
+    url = models.CharField(null=True, max_length=200)
+    notes = models.TextField(null=True)
+
+
+class Role(models.Model):
+    job_title = models.CharField(max_length=100)
+    salary = models.IntegerField(default=0)
+    job_notes = models.TextField(null=True)
+
+
+class Interview(models.Model):
+    interview_date = models.DateTimeField()
+    interview_notes = models.TextField()
